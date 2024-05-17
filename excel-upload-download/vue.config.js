b6acd69f6e73ed1c681f04465c4fded96ecdf37a
eungyu kim <rladmsrb111@gmail.com>
2024-05-17 23:24:03 +0900

    excel-upload-download setting update

--- excel-upload-download/vue.config.js
+++ excel-upload-download/vue.config.js
@@ -1,4 +1,24 @@
-const { defineConfig } = require('@vue/cli-service')
+// vue.config.js
+const { defineConfig } = require('@vue/cli-service');
+const webpack = require('webpack');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        crypto: require.resolve('crypto-browserify'),
+        fs: false,
+        stream: require.resolve('stream-browserify'),
+        buffer: require.resolve('buffer/'),
+        process: require.resolve('process/browser'), // process 폴리필 추가
+      },
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+        process: 'process/browser', // process 폴리필 추가
+      }),
+    ],
+  },
+});
